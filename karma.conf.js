524806a181b7a42b75277278828193ba8ce5a89b
Anderson Tavares <nocturne.pe@gmail.com>
2016-05-04 20:52:25 -0300

    retirando verbose e junit nos testes

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','verbose','junit','dots','coverage'],
+    reporters: ['progress','dots','coverage'],
 
 
     // web server port
