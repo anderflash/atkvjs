b3e3ee3ef7b187a79a4c803a765055c36d7000ff
Anderson Tavares <nocturne.pe@gmail.com>
2016-05-05 04:36:16 -0300

    criando os testes para ndarray

--- src/core/ndarray.ts
+++ src/core/ndarray.ts
@@ -1,7 +1,35 @@
-class NDArray<T extends ArrayLike<number>>{
+class NDArray{
   dim:number;
   size:Array<number>;
   step:Array<number>;
-  data:T;
-  
+  data:ArrayBuffer;
+  dtype: ArrayLike<number>;
+  num_elements: number;
+  elemsize: number;
+  constructor(data_array:ArrayLike<number>|Iterable<number>, data_size:Array<number>, dtype:any = null){
+    this.dim = data_size.length;
+    this.size = data_size;
+    this.step = Array<number>(this.dim);
+    this.step[this.dim-1] = 1;
+    for (let i = 1; i < this.dim; i++)
+      this.step[this.dim-i-1] = this.step[this.dim-i]*
+                                this.size[this.dim-i];
+    if(data_array === null){
+      this.data = data_array;
+    }else if(data_array instanceof ArrayLike<number>){
+      
+    }
+    
+    if(dtype === undefined){
+      this.dtype = new Uint32Array(this.data);
+    }
+  }
+  get(...items):number
+  {
+    let index:number = 0;
+    for (let i = 0; i < items.length; i++){
+      index += this.step[i] * items[i];
+    }
+    return this.dtype[index];
+  }
 }
--- tests/core/ndarray.spec.ts
+++ tests/core/ndarray.spec.ts
@@ -1,3 +1,30 @@
 describe('NDArray tests', ()=>{
+  var emptyarray = new NDArray(null,[2, 3]);
+  it("should have proper dimension", function(){
+    expect(emptyarray.dim).toBe(2);
+  });
+  it("should have proper size", function() {
+    expect(emptyarray.size[0]).toBe(2);
+    expect(emptyarray.size[1]).toBe(3);
+  });
+  it("should have proper step", function(){
+    expect(emptyarray.step[0]).toBe(3);
+    expect(emptyarray.step[1]).toBe(1);
+  });
+  it("should have type Int32Array", function(){
+    expect(emptyarray.dtype).toEqual(jasmine.any(Int32Array));
+  });
 
+  var data = [1, 2, 3, 4, 5, 6];
+  var array_with_value = new NDArray([1,2,3,4,5,6], [2, 3]);
+  it("should have proper data",function(){
+    for (let i = 0; i < data.length; i++){
+      expect(array_with_value.get(i)).toBe(data[i]);
+    }
+    for (let y = 0, i = 0; y < 2; y++){
+      for (let x = 0; x < 3; x++, i++){
+        expect(array_with_value.get(y, x)).toBe(data[i]);
+      }
+    }
+  });
 });
