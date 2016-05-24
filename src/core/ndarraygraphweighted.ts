6137caa2d0003e50edc9c14ec5c47b7c54cb9219
Anderson Tavares <nocturne.pe@gmail.com>
2016-05-23 21:37:28 -0300

    Adicionando arquivos para arraygraph weighted

---
+++ src/core/ndarraygraphweighted.ts
@@ -0,0 +1,6 @@
+/// <reference path="ndarraygraph.ts" />
+// module at{
+//   export class NDArrayGraphWeighted<T extends TypedArray<T>> extends NDArrayGraph<T>{
+//     weights: NDArray<Float32Array>;
+//   }
+// }
---
+++ tests/core/ndarraygraphweighted.spec.ts
@@ -0,0 +1,14 @@
+/// <reference path="../../typings/main/ambient/jasmine/index.d.ts" />
+/// <reference path="../../src/core/ndarraygraphweighted.ts" />
+
+// describe("Weighted NDArrayGraph tests", function() {
+//   it("should create an empty weighted graph array", function() {
+//     type tipo = Int32Array;
+//     var tipo_ctor = Int32Array;
+//     var grapharray: at.NDArrayGraphWeighted<tipo> = new at.NDArrayGraphWeighted(tipo_ctor, [3, 4]);
+//     expect(grapharray.num_neighbors).toBe(4);
+//     expect(grapharray.neighbors.size).toEqual([3, 4, 4]);
+//     expect(grapharray.edges.size).toEqual([3, 4, 4]);
+//     expect(grapharray.weights.size).toEqual([3, 4, 4]);
+//   });
+// });
